dfe3835b475562e5920006474b0be0fa9c9e31e8
jasonronalddavis <jdavis@c.ringling.edu>
2021-08-09 17:01:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () => {
+
+const container = document.querySelector("#dog-image-container")
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+fetch(imgUrl)
+.then(resp => resp.json())
+.then(images => {
+    const imgs = images.message
+    //retrieve array images
+    // turn it into  img element
+    // appennd to DOM
+let imgsArray = imgs.map((img) => {
+let i = '<img src=${img}'
+console.log(img)
+console.log(i)
+return i
+})
+
+
+})
+
+
+console.log(container)
+})
+
+
+
+
